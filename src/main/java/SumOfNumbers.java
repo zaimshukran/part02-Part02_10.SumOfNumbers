412673ab7af77f9c2770df629fca71a6c74e2c8b
Zaim <67372738+zaimshukran@users.noreply.github.com>
2023-12-27 14:46:28 +0800

    Initial commit

---
+++ src/main/java/SumOfNumbers.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int total = 0;
+
+        while(true){
+            System.out.println("Give a number: ");
+            int input = Integer.parseInt(scanner.nextLine());
+
+            if(input == 0){
+                break;
+            }
+
+            total += input;
+        }
+
+        System.out.println("Sum of the numbers: " + total);
+    }
+}
